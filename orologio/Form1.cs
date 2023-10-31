a8eb809441a37354412e31bf495f53b4358c0c68
andreaRoli <aggiustatuttobob4@gmail.com>
2023-10-31 15:58:00 +0100

    prova3

--- orologio/Form1.cs
+++ orologio/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.XPath;
 
 namespace orologio
 {
@@ -23,42 +24,48 @@ namespace orologio
 
             Graphics g = this.CreateGraphics();
 
-            
+
             Pen p = new Pen(Color.Black);
 
             // prendiamo heigh perche cosi resta nel form agevolmente, con width uscirebbe percge l'altezza sarebbe troppo grande (width>height)
             int diametro = ClientSize.Height;
             int raggio = diametro / 2;
 
-            
+
             int x = this.ClientSize.Width / 2 - raggio;
             int y = this.ClientSize.Height / 2 - raggio;
 
-           // Graphics ee= this.CreateGraphics();
-            g.DrawEllipse(p, x, y, diametro-3, diametro-3);
-            
-            
-            
-            // metodo 1    const double angolo = (5.0d / 4.0d) * Math.PI;
-           //for (int i = 0; i < 11; i++)
-            //{
-              //  float x1 = (float)Math.Cos(angolo - ((3.0d / 2.0d) * Math.PI / 10.0d) * i) * 195.0f + 200.0f;
-                //float y1 = (float)Math.Abs(Math.Sin(angolo - ((3.0d / 2.0d) * Math.PI / 10.0d) * i) * 195.0f - 200.0f);
-                //float x2 = (float)Math.Cos(angolo - ((3.0d / 2.0d) * Math.PI / 10.0d) * i) * 180.0f + 200.0f;
-                //float y2 = (float)Math.Abs(Math.Sin(angolo - ((3.0d / 2.0d) * Math.PI / 10.0d) * i) * 180.0f - 200.0f);
-                //ee.DrawLine(p, x1, y1, x2, y2);
+            // Graphics ee= this.CreateGraphics();
+            g.DrawEllipse(p, x, y, diametro - 3, diametro - 3);
 
-                //float x3 = (float)Math.Cos(angolo - ((3.0d / 2.0d) * Math.PI / 10.0d) * i) * 215.0f + 200.0f + pictureBox1.Location.X;
-                //float y3 = (float)Math.Abs(Math.Sin(angolo - ((3.0d / 2.0d) * Math.PI / 10.0d) * i) * 215.0f - 200.0f) + pictureBox1.Location.Y;
-            //}
+            //Coordinate centro circonferenza
+            int xc = this.ClientSize.Width / 2;
+            int yc = this.ClientSize.Height / 2;
+            g.FillEllipse(Brushes.Black, xc, yc, 8, 8);
 
-                //APPUNTI per operazioni matematiche : 
+            //Disegno lancette
+            DateTime t = new DateTime();
+            t = DateTime.Now;
 
-                //( 2PI*((x-10)/2) )/12  è la lunghezza dell'arco che forma ogni "minuto"
+            Graphics d = this.CreateGraphics();
+            Pen lancetta = new Pen(Color.Black);
+            int oraOrologio = t.Hour;
+            if (t.Hour > 12)
+            {
+                oraOrologio = oraOrologio - 12;
 
-                //l'angolo che forma è (2PI*(( 2PI*((x-10)/2) )/12) ) / ( PI*((x-10)/2) )/12)^2)
-                // l'angolo aumenta andando in senso orario 
 
             }
-    }
+
+
+                double xp = xc+raggio * Math.Cos(oraOrologio * (Math.PI / 6))+(Math.PI/2);
+                double yp = yc-raggio * Math.Sin(oraOrologio * (Math.PI / 6))+(Math.PI / 2);
+                d.DrawLine(lancetta, xc, yc, (float)xp+208, (float)yp+208);
+
+
+
+
+            }
+        }
+    
 }
