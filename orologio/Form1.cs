cc4a0282c58d501d6ae0ba745471805905a6a3f6
Tommaso Castagnoli <tommasocastagnoli29@gmail.com>
2023-11-01 19:23:14 +0100

    Prova 4

--- orologio/Form1.cs
+++ orologio/Form1.cs
@@ -57,15 +57,50 @@ namespace orologio
 
             }
 
-
                 double xp = xc+raggio * Math.Cos(oraOrologio * (Math.PI / 6))+(Math.PI/2);
                 double yp = yc-raggio * Math.Sin(oraOrologio * (Math.PI / 6))+(Math.PI / 2);
                 d.DrawLine(lancetta, xc, yc, (float)xp+208, (float)yp+208);
 
+            for (int i = 0; i < 12; i++)
+            {
+                // 30 gradi perche 360/12 e calcolo l'angolo
 
+                double angolo = i * (Math.PI / 6); 
+                // punto di partenza, usiamo UNA PARTE(88%) del raggio come lunghezza delle tacchette, xc e yc li  usiamo  per traslare le tacchette al centro dell'orologio
+                double xt = xc + (raggio*0.88) * Math.Cos(angolo);
+                double yt = yc - (raggio*0.88) * Math.Sin(angolo);
+                // punto di arrivo delle tacchette
+                double xt1 = xc + (raggio) * Math.Cos(angolo);
+                double yt1 = yc - (raggio) * Math.Sin(angolo);
 
-
+               
+                g.DrawLine(p, (float)xt, (float)yt, (float)xt1, (float)yt1);
             }
+
+
+            Pen q = new Pen(Color.BlueViolet);
+              
+            // 11 sono le tacchette tra ogni ora(5,10,15,20....55) moltiplicati per 15 per far si che ce ne siano 11 tra ogni ora 
+            for(int i = 0; i < 11*15; i++)
+            {
+
+                double angolominuti = i * ((Math.PI / 6)/12);
+              
+                double xtm = xc + (raggio * 0.96) * Math.Cos(angolominuti);
+                double ytm= yc - (raggio * 0.96) * Math.Sin(angolominuti);
+              
+                double xt1m = xc + raggio * Math.Cos(angolominuti);
+                double yt1m = yc - raggio * Math.Sin(angolominuti);
+
+                
+                g.DrawLine(q, (float)xtm, (float)ytm, (float)xt1m, (float)yt1m);
+            }
+
+
         }
+
+
+
+    }
     
 }
